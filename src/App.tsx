635722882a4c6b71be44a2c503537cd9982b1213
Alex Nascimento <alexjose68@hotmail.com>
2024-05-29 14:00:23 -0300

    feat: Conditional Rendering Example with Ternary Conditional Operator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [signed, setSigned] = useState(false);
 
   return (
     <div>
       <h1>Olá mundo</h1>
+      <br /><br />
+
+      {/* Renderização condicional com Operador Condicional Ternário: */}
+
+      {signed ? (
+        <div>
+          <h1>Bem vindo Alex José</h1> <br />
+          <button onClick={ () => setSigned(false) }>Fazer Logout</button>
+        </div>
+      ) : (
+        <h1>Nenhum usuário online</h1>
+      )}
+      <br /><br />
+
+      <button onClick={ () => setSigned(true) }>Logar usuário</button>
+
+      <br /><br />
+      <hr />
     </div>
   )
 }
